f7021a63c79d496538a207619daaf9b63a2a4fb5
Illya <i.khomenko@ct-college.net>
2019-01-10 22:48:01 +0200

    Basic navigation
    Simple navigation. Drop-down menu. 2 pages, 2 buttons.

--- track/track/track/MainPage.xaml.cs
+++ track/track/track/MainPage.xaml.cs
@@ -7,11 +7,33 @@ using Xamarin.Forms;
 
 namespace track
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
+           
             InitializeComponent();
+            Detail = new NavigationPage(new ORLOW())
+            {
+                BarBackgroundColor = Color.FromHex("#1c74da")
+            };
+            IsPresented = true;
+        }
+        private void Button1_Click(object sender,EventArgs e)
+        {
+            Detail = new NavigationPage(new ORLOW())
+            {
+                BarBackgroundColor = Color.FromHex("#1c74da")
+            };
+             IsPresented = false;
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new ORLOW_1())
+            {
+                BarBackgroundColor = Color.FromHex("#1c74da")
+            };
+            IsPresented = false;
         }
     }
 }
---
+++ track/track/track/ORLOW_1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace track
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ORLOW_1 : ContentPage
+	{
+		public ORLOW_1 ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
