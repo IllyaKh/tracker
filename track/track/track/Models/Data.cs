670824964c6f336a87886e0e99fa2246e1b8f977
IllyaKh <i.khomenko@outlook.com>
2019-02-09 10:21:10 +0200

    Data Table
    Adding cs code for Data table + auto id binding

---
+++ track/track/track/Models/Data.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+
+namespace track.Models
+{
+    class Data:User
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int SugarNow { get; set; }
+
+        public override string ToString(string local)
+        {
+            if (local == Convert.ToString(Id))
+                return (this.Id);
+            else if (local == Convert.ToString(UserId))
+                return Convert.ToSring(this.UserId);
+            else if (local == Convert.ToString(SugarNow))
+                return Convert.ToString(this.SugarNow);
+        }
+
+        public void BindingId(int idUser)
+        {
+            this.UserId = idUser;
+        }
+
+
+    }
+}
